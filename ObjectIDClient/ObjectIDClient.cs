d3abea880b6c8041f98c707897c43bd8cf2488ea
Brandon <brandonyuh@gmail.com>
2023-05-31 13:25:19 -0700

    Set up basic Remote Procedure Call to add one to a number

--- ObjectIDClient/ObjectIDClient.cs
+++ ObjectIDClient/ObjectIDClient.cs
@@ -1,2 +1,86 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Concurrent;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+public class RpcClient : IDisposable
+{
+    private const string QUEUE_NAME = "rpc_queue";
+
+    private readonly IConnection connection;
+    private readonly IModel channel;
+    private readonly string replyQueueName;
+    private readonly ConcurrentDictionary<string, TaskCompletionSource<string>> callbackMapper =
+        new();
+
+    public RpcClient()
+    {
+        var factory = new ConnectionFactory { HostName = "localhost" };
+
+        connection = factory.CreateConnection();
+        channel = connection.CreateModel();
+        // declare a server-named queue
+        replyQueueName = channel.QueueDeclare().QueueName;
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (model, ea) =>
+        {
+            if (!callbackMapper.TryRemove(ea.BasicProperties.CorrelationId, out var tcs))
+                return;
+            var body = ea.Body.ToArray();
+            var response = Encoding.UTF8.GetString(body);
+            tcs.TrySetResult(response);
+        };
+
+        channel.BasicConsume(consumer: consumer, queue: replyQueueName, autoAck: true);
+    }
+
+    public Task<string> CallAsync(string message, CancellationToken cancellationToken = default)
+    {
+        IBasicProperties props = channel.CreateBasicProperties();
+        var correlationId = Guid.NewGuid().ToString();
+        props.CorrelationId = correlationId;
+        props.ReplyTo = replyQueueName;
+        var messageBytes = Encoding.UTF8.GetBytes(message);
+        var tcs = new TaskCompletionSource<string>();
+        callbackMapper.TryAdd(correlationId, tcs);
+
+        channel.BasicPublish(
+            exchange: string.Empty,
+            routingKey: QUEUE_NAME,
+            basicProperties: props,
+            body: messageBytes
+        );
+
+        cancellationToken.Register(() => callbackMapper.TryRemove(correlationId, out _));
+        return tcs.Task;
+    }
+
+    public void Dispose()
+    {
+        connection.Close();
+    }
+}
+
+public class Rpc
+{
+    public static async Task Main(string[] args)
+    {
+        Console.WriteLine("RPC Client");
+        for (int i = 0; i < 25; i++)
+        {
+            InvokeAsync(i.ToString());
+        }
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+
+    private static async Task InvokeAsync(string n)
+    {
+        using var rpcClient = new RpcClient();
+
+        Console.WriteLine(" [x] Requesting AddOne({0})", n);
+        var response = await rpcClient.CallAsync(n);
+        Console.WriteLine(" [.] Got '{0}'", response);
+    }
+}
